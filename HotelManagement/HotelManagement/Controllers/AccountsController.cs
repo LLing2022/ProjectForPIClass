cbfd316140d9098068540dc06bab619fc78126d4
Gao xi <xigao670@gmail.com>
2023-10-05 19:23:57 -0400

    login

---
+++ HotelManagement/HotelManagement/Controllers/AccountsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security;
+using HotelManagement.Models;
+using System.Web.Security;
+using Membership = HotelManagement.Models.Membership;
+
+namespace HotelManagement.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Membership model1)
+        {
+            using (var context=new HotelManagementDBEntities ())
+            {
+                bool isValidUser=context.Users.Any(x=>x.name == model1.UserName && x.password == model1.UserPassword);
+                if (isValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(model1.UserName, false);
+                    return RedirectToAction("Index", "Users");
+                }
+               
+                ModelState.AddModelError("", "Invaild username and password");
+                return View();
+            }
+           
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model1)
+        {
+            using (var context=new HotelManagementDBEntities())
+            {
+                context.Users.Add(model1);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult StaffLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult StaffLogin(Staff model1)
+        {
+            using (var context = new HotelManagementDBEntities())
+            {
+                bool isValidStaff = context.Staffs.Any(s => s.name == model1.name && s.password == model1.password);
+                if (isValidStaff)
+                {
+                    FormsAuthentication.SetAuthCookie(model1.name, false);
+                    return RedirectToAction("Index", "Staffs");
+                }
+
+                ModelState.AddModelError("", "Invaild username and password");
+                return View();
+            }
+        }
+
+    }
+}
---
+++ HotelManagement/HotelManagement/Models/Membership.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagement.Models
+{
+    public class Membership
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }   
+        public string StaffEmail { get; set; }
+        public string StaffPassword { get; set; }
+
+        public string StaffPhonenumber { get; set; }
+        public bool IsManager { get; set; }
+
+
+    }
+}
