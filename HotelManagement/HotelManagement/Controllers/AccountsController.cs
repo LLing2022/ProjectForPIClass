991a20fba726fe0d65465843128a1f0b29e5b24b
Gao xi <xigao670@gmail.com>
2023-10-07 20:09:49 -0400

    login

--- HotelManagement/HotelManagement/Controllers/AccountsController.cs
+++ HotelManagement/HotelManagement/Controllers/AccountsController.cs
@@ -41,13 +41,28 @@ namespace HotelManagement.Controllers
         [HttpPost]
         public ActionResult Signup(User model1)
         {
+          
+            if (!IsPasswordValid(model1.password))
+            {
+                ModelState.AddModelError("", "Password needs to be at least 8 characters long and include at least one special character");
+                return View();
+            }
+
+            if (!IsValidEmail(model1.emailAddress))
+            {
+                ModelState.AddModelError("", "Invalid email format. Please use a valid email adress.");
+                return View();
+            }
             using (var context=new HotelManagementDBEntities())
             {
                 context.Users.Add(model1);
                 context.SaveChanges();
             }
             return RedirectToAction("Login");
+            
         }
+      
+
         public ActionResult StaffLogin()
         {
             return View();
@@ -55,19 +70,60 @@ namespace HotelManagement.Controllers
         [HttpPost]
         public ActionResult StaffLogin(Staff model1)
         {
+            if (!IsValidEmail(model1.email))
+            {
+                ModelState.AddModelError("", "Invalid email format. Please enter a valid email adress");
+                return View();
+            }
+           
             using (var context = new HotelManagementDBEntities())
             {
-                bool isValidStaff = context.Staffs.Any(s => s.name == model1.name && s.password == model1.password);
-                if (isValidStaff)
+                var user = context.Staffs.SingleOrDefault(s => s.email == model1.email);
+
+                if (user != null)
                 {
-                    FormsAuthentication.SetAuthCookie(model1.name, false);
-                    return RedirectToAction("Index", "Staffs");
+                    if (user.password == model1.password)
+                    {
+                        if (user.isManager)
+                        {
+                            // Manager login logic
+                            FormsAuthentication.SetAuthCookie(model1.email, false);
+                            return RedirectToAction("Index", "Staffs");
+                        }
+                        else
+                        {
+                            // Staff login logic
+                            FormsAuthentication.SetAuthCookie(model1.email, false);
+                            return RedirectToAction("Index", "Users");
+                        }
+                    }
                 }
 
-                ModelState.AddModelError("", "Invaild username and password");
+                ModelState.AddModelError("", "Invalid username or password");
                 return View();
             }
         }
 
+        private bool IsValidEmail(string email)
+        {
+            return email.ToLower().Contains("@");
+        }
+        private bool IsPasswordValid(string password)
+        {
+            return password.Length >= 8 && ContainsSpecialCharacter(password);
+        }
+        private bool ContainsSpecialCharacter(string input)
+        {
+            string specialCharacters = "!@#$%^&*()-+=[]{}|;:',.<>?/";
+            return input.Any(c => specialCharacters.Contains(c));
+        }
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            // Remove dashes and spaces from the phone number
+            string cleanedNumber = new string(phoneNumber.Where(char.IsDigit).ToArray());
+
+            // Check if the cleaned phone number is 10 digits long
+            return cleanedNumber.Length == 10;
+        }
     }
 }
